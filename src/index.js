de32697fdcb1048f3aacf8472450b582b678f534
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-01-29 23:28:48 +0600

    setup continues

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("app connection error : ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at the port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
 
 // (async () => {
 //     try {
